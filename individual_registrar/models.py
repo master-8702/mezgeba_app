d4ccca1549d875e4d3572f6645709a345b8136f0
master-8702 <55705389+master-8702@users.noreply.github.com>
2023-02-12 14:17:07 +0300

    add gender field to individual_registrar model

--- individual_registrar/models.py
+++ individual_registrar/models.py
@@ -7,12 +7,14 @@ from django.core.validators import RegexValidator
 
 class IndividualRegistrar(models.Model):
 
+    GENDER_CHOICE = (('male','Male'), ('female', 'Female'))
     # here we are creating regex (regular expression) validator fro our phone field to accepty numbers only
     # with a maximum of fiften (15) digit
     phone_regex = RegexValidator(regex=r'^[0-9]+$', message="Phone number must be numerics only")
 
     user =models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(unique=True,validators=[phone_regex], max_length=15) # validators should be a list
+    gender = models.CharField(choices=GENDER_CHOICE, max_length=6)
     address_country = models.CharField(max_length=255)
     address_state = models.CharField(max_length=255)
     address_city = models.CharField(max_length=255)
